f4f3804f3b450a8bd15fbc4ac09b1d29faf88a98
Chirag30Sharma <chirag30sharma@gmail.com>
2023-04-24 21:06:05 +0530

    first commit

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+$con=mysqli_connect("sql12.freemysqlhosting.net","sql12613371","5KuMILTKyQ","sql12613371");
+
+/* if($con){
+    echo "connection successfull";
+}else{
+    echo "connection error".mysqli_connect_error();
+}*/
+
+?>
