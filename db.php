8c422fbfc2a376836c81b9dcb7fd953fbb647f02
Chirag30Sharma <chirag30sharma@gmail.com>
2023-04-25 10:18:22 +0530

    Database changed

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("sql12.freemysqlhosting.net","sql12613371","5KuMILTKyQ","sql12613371");
+$con=mysqli_connect("localhost","sql12613371","","sql12613371");
 
 /* if($con){
     echo "connection successfull";
